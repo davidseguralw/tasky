23c5b14bcf772f77a3ecc597cf816c2418b9edb6
davidseguralw <david.segura@lacework.net>
2025-11-16 08:42:37 +0000

    auto-backup

--- iac2/20-mongo/locals.tf
+++ iac2/20-mongo/locals.tf
@@ -9,7 +9,23 @@ locals {
   }
 
   # Use explicit backup bucket if set, otherwise default convention
-  bucket_name = var.backup_bucket_name != "" ?
-    var.backup_bucket_name :
-    "${var.project}-mongo-backups-${var.aws_region}"
+  bucket_name = var.backup_bucket_name != "" ? var.backup_bucket_name : "${var.project}-mongo-backups-${var.aws_region}"
 }
+
+
+
+# locals {
+#   # Include env in names
+#   name_prefix = "${var.project}-${var.environment_name}-${var.aws_region}"
+
+#   tags = {
+#     Project     = var.project
+#     Component   = "mongo"
+#     Environment = var.environment_name
+#   }
+
+#   # Use explicit backup bucket if set, otherwise default convention
+#   bucket_name = var.backup_bucket_name != "" ?
+#     var.backup_bucket_name :
+#     "${var.project}-mongo-backups-${var.aws_region}"
+# }
